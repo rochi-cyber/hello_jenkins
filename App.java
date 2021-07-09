11a779284e4371125b3966e86ac3905ce61c05fd
rochi-cyber <57502538+rochi-cyber@users.noreply.github.com>
2021-07-09 16:45:50 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,6 @@
+package com.acc.app;
+class App{
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ AppTest.java
@@ -0,0 +1,5 @@
+class AppTest{
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
